0008989eae4973cc027ea63513cb4fee6052c8d1
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2023-06-30 15:07:10 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,4 +1,31 @@
 class Solution {
+    public static int answer;
+    public static int call=0;
+    public static int[] nums;
+    public static void getAnswer(int index,int sum,int count){
+        if(count==3){
+            call++;
+            if(sum==0){
+                answer++;
+            }
+        }else if(index<nums.length){
+            getAnswer(index+1,sum+nums[index],count+1);
+            getAnswer(index+1,sum,count);
+        }
+    }
+
+    public int solution(int[] number) {
+        answer = 0;
+        nums=new int[number.length];
+        for(int i=0;i<number.length;i++){
+            nums[i]=number[i];
+        }
+        getAnswer(0,0,0);
+        return answer;
+    }
+}
+/*
+class Solution {
     public int solution(int[] number) {
         int result = 0;
         for (int i=0; i<number.length; i++) {
@@ -12,3 +39,4 @@ class Solution {
         return result;
     }
 }
+*/
