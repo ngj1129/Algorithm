0ec09d6d9a442f2f8454b752a0fbb71cd91f4691
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2023-08-03 22:31:38 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,5 @@
+select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date, '%Y-%m-%d') as created_date
+from used_goods_board b, used_goods_reply r
+where b.board_id = r.board_id and
+      date_format(b.created_date, '%Y-%m') = '2022-10'
+order by r.created_date asc, b.title asc;
