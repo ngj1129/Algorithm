bc7e987db7bde987d9788be9eb25ae20c142d5a1
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2023-09-04 16:12:31 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int l = Integer.parseInt(br.readLine());
+    
+        String s = new String(br.readLine());
+        int sum = 0;
+        for (int i=0; i<s.length(); i++) {
+            int num = ((int) s.charAt(i) - 96) * (int) Math.pow(31, i);
+            sum += num;
+        }
+        System.out.println(sum);
+
+
+        
+    }
+}
