05ab71637df82587ff3f3f769317a34eb2ee45f0
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2023-10-10 22:29:37 +0900

    [Gold III] Title: 아기 상어, Time: 212 ms, Memory: 25756 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,90 +1,101 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.ArrayList;
+
 public class Main {
-    
-    static int n;
-    static int[][] board;
-    static int dx[] = {-1, 0, 1, 0}; //위 왼 아래 오
-    static int dy[] = {0, 1, 0, -1};
-    static ArrayList<Node> fishes;
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        n = scan.nextInt(); 
-        board = new int[n][n];
-        Queue<Node> q = new LinkedList<>();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                board[i][j] = scan.nextInt();
-                if(board[i][j] == 9){
-                    q.add(new Node(i, j, 0));
-                    board[i][j] = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] place = new int[N][N];
+        int[] start = new int[2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                place[i][j] = Integer.parseInt(st.nextToken());
+                if (place[i][j] == 9) {
+                    start[0] = i;
+                    start[1] = j;
+                    place[i][j] = 0;
                 }
             }
         }
-        
-        int eat = 0;
-        int time = 0; 
-        int age = 2;
-        while(true){
-            LinkedList<Node> fish = new LinkedList<>();
-            int[][] dist = new int[n][n];
- 
+        System.out.println(bfs(N, place, start));
+    }
+
+    public static int bfs(int N, int[][] place, int[] start) {
+        int lv = 2;
+        int ct = 0;
+        int time = 0;
+        int answer = 0;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] v = new boolean[N][N];
+        int[][] move = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        ArrayList<int[]> list = new ArrayList<>();
+        q.offer(new int[] { start[0], start[1], 0});
+        v[start[0]][start[1]] = true;
+        while (true) {
             while (!q.isEmpty()) {
-                Node current = q.poll();
-                
-                for(int i=0; i<4; i++){
-                    int nx = current.x + dx[i];
-                    int ny = current.y + dy[i];
-                 
-                    if(nx >= 0 && ny >= 0 && nx < n && ny < n && dist[nx][ny]==0 && board[nx][ny] <= age){
-                        dist[nx][ny] = dist[current.x][current.y] + 1;
-                        q.add(new Node(nx, ny, dist[nx][ny]));
-                        if(1 <= board[nx][ny] && board[nx][ny] <= 6 && board[nx][ny] < age) fish.add(new Node(nx, ny, dist[nx][ny]));
-                    }
-                }
-            }
- 
-            if(fish.size() == 0){
-                System.out.println(time);
-                return;
-            }
- 
-            Node currentFish = fish.get(0);
-            for(int i = 1; i < fish.size(); i++){
-                if(currentFish.dist > fish.get(i).dist) {
-                    currentFish = fish.get(i);
-                }
-                else if(currentFish.dist == fish.get(i).dist) {
-                    if(currentFish.x > fish.get(i).x) currentFish = fish.get(i);
-                    else if(currentFish.x == fish.get(i).x){
-                        if(currentFish.y > fish.get(i).y) currentFish = fish.get(i);   
-                    }
-                }   
-            }
-             
-            time += currentFish.dist;
-            eat++;
-            board[currentFish.x][currentFish.y] = 0;
-            if(eat == age) {
-                age++;
-                eat = 0;
-            }
-            q.add(new Node(currentFish.x, currentFish.y, 0));
-        }
-    }
+                int[] tmp = q.poll();
+                int x = tmp[0];
+                int y = tmp[1];
+                time = tmp[2];
     
-    public static class Node {
-        int x;
-        int y;
-        int dist;
-        
-        public Node(int x, int y, int dist) {
-            super();
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
+                for (int[] m: move) {
+                    if (x+m[0]>=0 && x+m[0]<N && y+m[1]>=0 && y+m[1]<N) {
+                        if (!v[x+m[0]][y+m[1]] && lv >= place[x+m[0]][y+m[1]]) { //들어가거나 먹을수있으면
+                            q.offer(new int[]{x+m[0], y+m[1], time+1});
+                            v[x+m[0]][y+m[1]] = true;
+                            if (place[x+m[0]][y+m[1]] > 0 && lv > place[x+m[0]][y+m[1]]) {//들어갈수있으면
+                                list.add(new int[]{x+m[0], y+m[1], time+1});
+                            }
+                        }
+                    }
+                }
+            }
+            if (list.size() == 0) {
+                return answer;
+            }
+                
+            int mx = list.get(0)[0];
+            int my = list.get(0)[1];
+            int d = list.get(0)[2];
+            for (int i=1; i<list.size(); i++) {
+                if (list.get(i)[2] < d) {
+                    mx = list.get(i)[0];
+                    my = list.get(i)[1];
+                    d = list.get(i)[2];
+                }
+                else if (list.get(i)[2] == d) {
+                    if (list.get(i)[0] < mx) {
+                        mx = list.get(i)[0];
+                        my = list.get(i)[1];
+                        d = list.get(i)[2];
+                    }
+                    else if (list.get(i)[0] == mx) {
+                        if (list.get(i)[1] < my) {
+                            mx = list.get(i)[0];
+                            my = list.get(i)[1];
+                            d = list.get(i)[2];
+                        }
+                    }
+                }
+            }
+            answer = d;
+            ct++;
+            if (ct == lv) {
+                lv++;
+                ct = 0;
+            }
+            //q.clear();
+            list = new ArrayList<int[]>();
+            v = new boolean[N][N];
+            place[mx][my] = 0;
+            q.offer(new int[]{mx, my, d});
+            v[mx][my] = true;
         }
     }
-}  
+}
