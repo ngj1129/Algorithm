46ad1f34fadc21c405faea87a0660d4ff2d349c7
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2023-10-20 15:40:06 +0900

    [Silver II] Title: 좌표 압축, Time: 1980 ms, Memory: 276984 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -3,40 +3,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.ArrayList;
 
 public class Main {
+    public static boolean flag = false;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] arr = new int[n];
+        int[] s_arr = new int[n];
         for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            set.add(arr[i]);
-        }
-        int[] s_arr = new int[set.size()];
-        int ix = 0;
-        Iterator<Integer> iter = set.iterator();
-        while (iter.hasNext()) {
-            s_arr[ix] = iter.next();
-            ix++;
+            s_arr[i] = arr[i];
         }
         Arrays.sort(s_arr);
-        for (int i=0; i<s_arr.length; i++) {
-            map.put(s_arr[i], i);
+        int i = 0;
+        int j = 1;
+        int ix = 0;
+        while (j < n) {
+            if (s_arr[i] == s_arr[j]) {
+                j++;
+            }
+            else {
+                map.put(s_arr[i], ix);
+                i = j;
+                j = i+1;
+                ix++;
+            }
         }
+        map.put(s_arr[i], ix);
         StringBuilder sb = new StringBuilder();
-
         for (int num: arr) {
             sb.append(map.get(num) + " ");
         }
         System.out.println(sb.toString());
-    }      
+    }
 }
