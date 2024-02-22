99cd39f88817def352d9f8d9caa70f2971641e00
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2024-02-22 16:43:57 +0900

    [level 3] Title: 단속카메라, Time: 8.15 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,6 +2,24 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Solution {
+    public int solution(int[][] targets) {
+        Arrays.sort(targets, (a, b) -> a[0] - b[0]); // x좌표 기준으로 정렬
+        int cnt = 0;
+        int last = -30001;
+        for (int i = 0; i < targets.length; i++) {
+            int[] missile = targets[i];
+            if (missile[0] > last) { // 새로운 요격 미사일 필요
+                cnt++;
+                last = missile[1]; // 해당 미사일로 커버되는 범위
+            } else if (missile[1] < last) { // 더 작은 범위로 커버 가능한 미사일 필요
+                last = missile[1]; // 해당 미사일로 커버되는 범위
+            }
+        }
+        return cnt;
+    }
+}
+/*
+class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
@@ -24,3 +42,4 @@ class Solution {
         return answer;
     }
 }
+*/
