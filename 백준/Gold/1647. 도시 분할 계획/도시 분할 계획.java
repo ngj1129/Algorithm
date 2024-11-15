3e067104b4624a7c7f85a4036907ef462a38c215
ngj1129 <109222760+ngj1129@users.noreply.github.com>
2024-11-15 15:48:03 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2724 ms, Memory: 348596 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,73 +1,95 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int V, E;
-	static int[][] graph;
-	// 각 노드의 부모
-	static int[] parent;
-	// 최종적으로 연결된 최소 신장 트리 연결 비용.
-	static int final_cost;
-    static int max;
-
+	
+	public static int N;
+	public static int M;
+	public static int[][] graph;
+	public static int[] parent;
+	public static int cost;
+	public static int last;
+	
+	public static class com implements Comparator<int[]> {
+		@Override
+		public int compare(int[] arr1, int[] arr2) {
+			return Integer.compare(arr1[2], arr2[2]);
+		}
+	}
+			
 	public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 그래프의 연결상태(노드1, 노드2, 비용)를 초기화.
+		//System.setIn(new FileInputStream("src/mst1/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		graph = new int[E][3];
-		for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new int[M][3];
+		parent = new int[N];
+		cost = 0;
+		last = 0;
+		HashMap<Integer, Integer> nodeMap = new HashMap<>();
+		int ix = 0;
+		
+		for (int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
 			graph[i][0] = Integer.parseInt(st.nextToken());
 			graph[i][1] = Integer.parseInt(st.nextToken());
 			graph[i][2] = Integer.parseInt(st.nextToken());
-		}
-		parent = new int[V];
-		final_cost = 0;
-
-		// 간선 비용 순으로 오름차순 정렬
-		Arrays.sort(graph, (o1, o2) -> Integer.compare(o1[2], o2[2]));
-
-		// makeSet
-		for (int i = 0; i < V; i++) {
-			parent[i] = i;
-		}
-		// 낮은 비용부터 크루스칼 알고리즘 진행
-		for (int i = 0; i < E; i++) {
-			// 사이클이 존재하지 않는 경우에만 간선을 선택한다(여기서는 최종 비용만 고려하도록 하겠다).
-			if (find(graph[i][0] - 1) != find(graph[i][1] - 1)) {
-				//System.out.println("<선택된 간선>");
-				//System.out.println(Arrays.toString(graph[i]));
-				union(graph[i][0] - 1, graph[i][1] - 1);
-				final_cost += graph[i][2];
-                if (graph[i][2]>max) {
-                    max = graph[i][2];
-                }
-				//System.out.println("<각 노드가 가리키고 있는 부모>");
-				//System.out.println(Arrays.toString(parent) + "\n");
-				continue;
+			if (nodeMap.containsKey(graph[i][0])) {
+				graph[i][0] = nodeMap.get(graph[i][0]);
+			}
+			else {
+				nodeMap.put(graph[i][0], ix);
+				graph[i][0] = ix;
+				ix++;
+			}
+			if (nodeMap.containsKey(graph[i][1])) {
+				graph[i][1] = nodeMap.get(graph[i][1]);
+			}
+			else {
+				nodeMap.put(graph[i][1], ix);
+				graph[i][1] = ix;
+				ix++;
 			}
 		}
-		System.out.println(final_cost-max);
-	}
-
-	private static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if (a > b) {
-			parent[a] = b;
-		} else {
-			parent[b] = a;
+		
+		for (int i=0; i<N; i++) {
+			parent[i] = i;
 		}
+		
+		Arrays.sort(graph, new com());
+		
+		for (int i=0; i<M; i++) {
+			int[] cur = graph[i];
+			int nodeA = cur[0];
+			int nodeB = cur[1];
+			int parentA = find(nodeA);
+			int parentB = find(nodeB);
+			if (parentA != parentB) {
+				union(parentA, parentB);
+				cost += cur[2];
+				last = cur[2];
+			}
+		}
+		
+		System.out.println(cost-last);
+		
 	}
-
-	private static int find(int x) {
-		if (parent[x] == x)
-			return x;
-		else
-			return parent[x] = find(parent[x]);
+	
+	public static int find(int node) {
+		if (parent[node] == node) {
+			return node;
+		}
+		return parent[node] = find(parent[node]);
+	}
+	
+	public static void union(int nodeA, int nodeB) {
+		if (nodeA < nodeB) {
+			parent[nodeB] = nodeA;
+		}
+		else {
+			parent[nodeA] = nodeB;
+		}
 	}
 }
